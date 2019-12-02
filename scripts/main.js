a3430cff0557f28ee316ea1809b93e617771a641
Alsubaie89 <A_H_3_D_89@hotmail.com>
2019-12-02 17:23:48 +0300

    recent update with sett=TimeOut

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,53 @@
 let chWinnerX = function(){
   //rows
     if ($('#b1-1').text() == "X" && $('#b1-2').text() == "X" && $('#b1-3').text() == "X"){
-    alert('x win') }
+    
+    setTimeout(function(){
+        alert('x win')   
+    },500)
+
+    }
 
     else if ($('#b2-1').text() == "X" && $('#b2-2').text() == "X" && $('#b2-3').text() == "X") {
-        alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
     else if ($('#b3-1').text() == "X" && $('#b3-2').text() == "X" && $('#b3-3').text() == "X") {
-        alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
   
   //columns
         else if ($('#b1-3').text() == "X" && $('#b2-3').text() == "X" && $('#b3-3').text() == "X"){
-    alert('x win') }
+            setTimeout(function(){
+                alert('x win')   
+            },500)
+         }
 
     else if ($('#b1-2').text() == "X" && $('#b2-2').text() == "X" && $('#b3-2').text() == "X") {
-        alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
     else if ($('#b1-1').text() == "X" && $('#b2-1').text() == "X" && $('#b3-1').text() == "X") {
-        alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
  
         
   // crossing
     else if ($('#b1-3').text() == "X" && $('#b2-2').text() == "X" && $('#b3-1').text() == "X") {
-    alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
     else if ($('#b1-1').text() == "X" && $('#b2-2').text() == "X" && $('#b3-3').text() == "X") {
-    alert('x win')
+        setTimeout(function(){
+            alert('x win')   
+        },500)
         }
 
 
@@ -38,34 +58,52 @@ let chWinnerO = function(){
     
     //rows
        if ($('#b1-1').text() == "O" && $('#b1-2').text() == "O" && $('#b1-3').text() == "0"){
-       alert('O wins') }
+        setTimeout(function(){
+            alert('O win')   
+        },500)
+     }
    
        else if ($('#b2-1').text() == "O" && $('#b2-2').text() == "O" && $('#b2-3').text() == "O") {
-           alert('O wins')
+        setTimeout(function(){
+            alert('O win')   
+        },500)
            }
        else if ($('#b3-1').text() == "O" && $('#b3-2').text() == "O" && $('#b3-3').text() == "O") {
-           alert('O wins')
+        setTimeout(function(){
+            alert('O win')   
+        },500)
            }
      
            
   //columns
        else if ($('#b1-3').text() == "O" && $('#b2-3').text() == "O" && $('#b3-3').text() == "O"){
-       alert('win') }
+        setTimeout(function(){
+            alert('O win')   
+            },500)
+         }
    
        else if ($('#b1-2').text() == "O" && $('#b2-2').text() == "O" && $('#b3-2').text() == "O") {
-           alert('O wins')
+        setTimeout(function(){
+            alert('O win')   
+            },500)
            }
        else if ($('#b1-1').text() == "O" && $('#b2-1').text() == "O" && $('#b3-1').text() == "O") {
-           alert('O wins')
+        setTimeout(function(){
+            alert('O win')   
+            },500)
         }
      
 
     //crossing
        else if ($('#b1-3').text() == "O" && $('#b2-2').text() == "O" && $('#b3-1').text() == "O") {
-       alert('O wins')
+        setTimeout(function(){
+            alert('O win')   
+            },500)
         }
         else if ($('#b1-1').text() == "O" && $('#b2-2').text() == "O" && $('#b3-3').text() == "O") {
-       alert('O wins')
+            setTimeout(function(){
+                alert('O win')   
+            },500)
        }
     
    
